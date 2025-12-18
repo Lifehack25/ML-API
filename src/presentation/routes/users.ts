bc8b138d2bc72f6e4cdbf43f0ffe8f36b182722b
Lifehack25 <pontus.dorsay@gmail.com>
2025-12-18 12:28:10 +0100

    same

--- src/presentation/routes/users.ts
+++ src/presentation/routes/users.ts
@@ -23,10 +23,6 @@ const createJwtMiddleware = (config: AppConfig) =>
   jwt({
     secret: config.jwt.secret,
     alg: "HS256",
-    verification: {
-      iss: config.jwt.issuer,
-      aud: config.jwt.audience,
-    },
   });
 
 //---------------------------------------------- Validation ----------------------------------------------
